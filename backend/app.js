cf76170c8ec28ff51a9120736a6bab9ca98e7753
Connor Nolan <connornolandev@gmail.com>
2024-06-18 18:47:40 -0700

    backend: add helmet, rate limiting, and NoSQL sanitization

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
+const helmet = require('helmet');
 const express = require('express');
 const morgan = require('morgan');
-const stripAnsi = require('./utils/stripAnsi');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 const { xss } = require('express-xss-sanitizer');
+
+const stripAnsi = require('./utils/stripAnsi');
 
 const errorController = require('./controllers/errorController');
 
 const AppError = require('./utils/appError');
 const logger = require('./utils/logger');
 
+const apiBaseUrl = '/api';
+const apiVersion = '/v1';
+
 const app = express();
+
+// set security http headers
+app.use(helmet());
 
 // http logging
 if (process.env.NODE_ENV === 'development') {
@@ -21,8 +31,19 @@ if (process.env.NODE_ENV === 'development') {
   );
 }
 
+// limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later',
+});
+app.use(apiBaseUrl, limiter);
+
 // body parser
 app.use(express.json({ limit: '10kb' }));
+
+// NoSQL query sanitization
+app.use(mongoSanitize());
 
 // xss sanitization
 app.use(xss());
